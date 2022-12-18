0ca119c9bc1284efd61456aaa75a85a95fd72b08
atordvairn <atordvairn@gmail.com>
2022-12-18 20:18:12 +0530

    awd

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -39,6 +39,13 @@
   });
 </script>
 
+<svelte:head>
+  <title>retrounhash</title>
+  <meta
+    name="description"
+    content="retrounhash is a decentralized social media platform to share, interact and socialise around with the world while staying secure and private."
+  />
+</svelte:head>
 <div class="flex h-screen bg-[#141414] text-white text-opacity-75">
   <div
     class="h-full w-12 flex flex-col gap-2 bg-[#19191a] text-white text-opacity-70"
