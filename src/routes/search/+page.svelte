52184a8cd53f61dbb72400358d361e93c42923f4
atordvairn <atordvairn@gmail.com>
2022-12-19 16:12:43 +0530

    auto submit

--- src/routes/search/+page.svelte
+++ src/routes/search/+page.svelte
@@ -10,6 +10,7 @@
   let posts = [];
 
   function search(query) {
+    posts = [];
     query.split(" ").forEach((query) => {
       query = query.replace(/ /, "");
       if (!(query.length <= 2)) {
@@ -76,22 +77,31 @@
     >
       <Search width="1.2em" />
     </div>
-    <input
-      bind:value={q}
-      type="text"
-      contenteditable="true"
-      placeholder="Search"
-      class="text-sm font-thin w-full resize-none bg-[#19191a] rounded p-1"
-    />
-    <!-- svelte-ignore a11y-click-events-have-key-events -->
-    <div
-      on:click={() => {
+    <form
+      autocomplete="off"
+      on:submit|preventDefault={() => {
         search(q);
       }}
-      class="h-6 flex justify-center w-6 m-auto aspect-square object-cover rounded-md"
+      class="flex w-full"
     >
-      <Arrow width="1.2em" />
-    </div>
+      <input
+        bind:value={q}
+        type="text"
+        contenteditable="true"
+        placeholder="Search"
+        class="text-sm font-thin w-full resize-none bg-[#19191a] rounded p-1"
+      />
+      <!-- svelte-ignore a11y-click-events-have-key-events -->
+      <button
+        on:click={() => {
+          search(q);
+        }}
+        type="submit"
+        class="h-6 flex justify-center items-center w-6 m-auto aspect-square object-cover rounded-md"
+      >
+        <Arrow width="1.2em" />
+      </button>
+    </form>
   </div>
 </div>
 
