1f03355897231596eda79a35536eeb6dbbb50bbc
atordvairn <atordvairn@gmail.com>
2022-12-20 09:51:38 +0530

    update

--- src/routes/dm/[slug]/+page.svelte
+++ src/routes/dm/[slug]/+page.svelte
@@ -71,15 +71,17 @@
         })
         .then((data) => {
           q = "";
-          messages = [
-            {
-              message: data.message,
-              time: Gun.state.is(data, "message"),
-              self: true,
-            },
-            ...messages,
-          ];
-          dummy.scrollIntoView();
+          // messages = [
+          //   {
+          //     message: data.message,
+          //     time: Gun.state.is(data, "message"),
+          //     self: true,
+          //   },
+          //   ...messages,
+          // ];
+          if (dummy) {
+            dummy.scrollIntoView();
+          }
         });
     }
   }
