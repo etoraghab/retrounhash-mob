6697c872851a5724e4736038a3241f1142533c9a
atordvairn <atordvairn@gmail.com>
2023-01-01 02:30:02 +0530

    awd

--- src/comp/post.svelte
+++ src/comp/post.svelte
@@ -1,4 +1,5 @@
 <script>
+  import { TrashAlt, UserPlus } from "@svicons/boxicons-regular";
   import { goto } from "$app/navigation";
   import { user } from "$lib/gun";
   import Menu from "@svicons/boxicons-regular/dots-horizontal-rounded.svelte";
@@ -9,6 +10,29 @@
   let option;
   export let data;
   let elmUID;
+
+  let content = data.content.replace(
+    /#+([a-zA-Z0-9_]+)/gi,
+    '<a href="/search/#$1">#$1</a>'
+  );
+
+  import { parse } from "twemoji-parser";
+  const entities = parse(content);
+
+  entities.forEach((e) => {
+    content = content.replace(
+      new RegExp(e.text, "g"),
+      `<img src="${e.url}" draggable="false" class="h-3 my-auto selector" alt="" />`
+    );
+  });
+
+  let following;
+  user
+    .get("following")
+    .get(data.pub)
+    .once((isFollowing) => {
+      following = isFollowing || false;
+    });
 </script>
 
 <div
@@ -16,45 +40,69 @@
   class="w-11/12 p-3 bg-[#ffffff] dark:bg-[#19191a] border border-[#dce1e6] dark:border-[#313131] rounded-md h-auto flex flex-col"
 >
   {#if option}
-    <div class="text-sm font-thin flex gap-1 items-center justify-center">
-      <button
-        class="flex px-2 py-1 rounded-md bg-[#e6e8eb] dark:bg-[#222222] dark:text-white gap-1 justify-center items-center"
-      >
-        <span class="text-red-600">
-          <Report width="1.2em" />
-        </span>
-        report
-      </button>
-      {#if data.self}
+    <div class="flex items-center p-2">
+      <div class="flex flex-col gap-1">
+        {#if data.self}
+          <button
+            class="flex text-red-500 gap-1 transition-all duration-300 text-sm p-1 dark:hover:bg-[#474040] hover:bg-[#f2f3f5] rounded-lg my-auto px-3"
+            on:click={async () => {
+              await user
+                .get("posts")
+                .get(data.uid)
+                .put(null)
+                .then(async () => {
+                  await user
+                    .get("searchable")
+                    .get(data.uid)
+                    .put(null)
+                    .then(() => {
+                      elmUID.remove();
+                    });
+                });
+            }}
+          >
+            <TrashAlt width="1.2em" />
+            Delete
+          </button>
+        {:else}
+          <div>
+            {#if following}
+              <button
+                on:click={() => {
+                  user.get("following").get(data.pub).put(false);
+                  following = false;
+                }}
+                class="flex gap-1 transition-all duration-300 text-sm p-1 dark:hover:bg-[#424242] hover:bg-[#f2f3f5] rounded-lg my-auto px-3"
+              >
+                <UserPlus width="1.2em" />
+                Unfollow @{data.name}
+              </button>
+            {:else}
+              <button
+                on:click={() => {
+                  user.get("following").get(data.pub).put(true);
+                  following = true;
+                }}
+                class="flex gap-1 transition-all duration-300 text-sm p-1 dark:hover:bg-[#424242] hover:bg-[#f2f3f5] rounded-lg my-auto px-3"
+              >
+                <UserPlus width="1.2em" />
+                Follow @{data.name}
+              </button>
+            {/if}
+          </div>
+        {/if}
         <button
-          class="flex px-2 py-1 rounded-md bg-[#e6e8eb] dark:bg-[#222222] dark:text-white gap-1 justify-center items-center"
-          on:click={async () => {
-            await user
-              .get("posts")
-              .get(data.uid)
-              .put(null)
-              .then(async () => {
-                await user
-                  .get("searchable")
-                  .get(data.uid)
-                  .put(null)
-                  .then(() => {
-                    elmUID.remove();
-                  });
-              });
-          }}
+          class="flex gap-1 transition-all duration-300 text-sm p-1 dark:hover:bg-[#424242] hover:bg-[#f2f3f5] rounded-lg my-auto px-3"
         >
-          <span class="text-red-600">
-            <Delete width="1.2em" />
-          </span>
-          delete
+          <Report width="1.2em" />
+          Report
         </button>
-      {/if}
+      </div>
       <button
         on:click={() => {
           option = false;
         }}
-        class="p-1 bg-[#e6e8eb] dark:bg-[#222222] dark:text-white rounded-md my-auto ml-auto"
+        class="ml-auto mr-2 mb-auto mt-2 flex gap-1 transition-all duration-300 text-sm p-1 dark:hover:bg-[#424242] hover:bg-[#f2f3f5] rounded-lg cursor-pointer"
       >
         <X width="1.2em" />
       </button>
